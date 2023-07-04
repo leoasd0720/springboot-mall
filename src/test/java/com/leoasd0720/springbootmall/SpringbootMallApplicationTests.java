fc45bd7142707dcf8ab573cdb6ec11e0581e706a
DESKTOP-PCTMEQ9\Lambert <leodevil13@gmail>
2023-07-04 16:33:51 +0800

    初始化 Spring Boot project

---
+++ src/test/java/com/leoasd0720/springbootmall/SpringbootMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leoasd0720.springbootmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
